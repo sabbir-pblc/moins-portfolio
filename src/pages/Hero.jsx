db9f072d75d1bb05ffcc68cf4b338dfd56cac71e
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-06-11 22:42:55 +0600

    edit

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion'
+
 const Hero = () => {
   return (
     <motion.div className='h-full' initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ ease: 'easeOut' }}>
@@ -8,11 +9,13 @@ const Hero = () => {
           <img src="https://via.placeholder.com/200x200" alt='' className='rounded-full' />
         </div>
 
-        <div className='space-y-4 w-3/5 mx-auto'>
+        <div className='space-y-4 lg:w-3/5 w-4/5 mx-auto'>
           <h2 className='text-xl font-medium'>Lorem ipsum dolor sit amet.</h2>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum illo ab consequatur eos blanditiis doloremque, rerum dignissimos saepe provident reprehenderit!
-          </p></div>
+          </p>
+          <a href="https://docs.google.com" target='_blank' rel="noopener noreferrer"><button className='mt-4 bg-black border px-8 text-white py-2 hover:bg-transparent hover:border-black hover:text-black transition-colors'>resume</button></a>
+        </div>
       </div>
     </motion.div>
   );
